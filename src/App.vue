5405e8e531900ed5abb585ad7be689db26b46099
Panupat Kammahawong (New) <new.panupat@maqe.com>
2021-11-01 18:20:41 +0700

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<div>
+		<ul>
+			<li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+			<li><router-link :to="{ name: 'Me' }">Me</router-link></li>
+		</ul>
+		<router-view />
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/views/Me.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Me.vue</h1>
+</template>
